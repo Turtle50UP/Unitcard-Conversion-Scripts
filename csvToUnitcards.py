22d724748a4b5b907be909181cb19abd1f2fad2b
Turtle50UP <turtle50vp@gmail.com>
2019-12-27 21:51:40 +0900

    working csv to unitcard v2 converter

--- csvToUnitcards.py
+++ csvToUnitcards.py
@@ -4,20 +4,36 @@ def modifyFilename(filename, string):
     # modifies filename to include string after filename but before extension
     # assumes provided string for filename has an extension
     index = filename.rfind(".")
-    return filename[:index] + "_" + "string" + filename[index:]
+    return filename[:index] + "_" + string + filename[index:]
+
+def changeExtension(filename, extension):
+    index = filename.rfind(".")
+    return filename[:index] + "." + extension
 
 def csvToUnitcard(filename):
     # takes a csv of data to populate unitcards, outputs a .txt file of unitcards
-    #TODO
-    with open(filename, 'r') as csvfile:
-        csvreader = csv.reader(csvfile)
+    # must be formatted exactly like example csv, including header
+    with open(filename, 'r', encoding='utf8') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=",")
         next(csvreader)
-        newFilename = modifyFilename(filename, "unitcardV2")
-        with open(filename, 'w') as unitcardfile:
+        newFilename = changeExtension(modifyFilename(filename, "unitcardV2"), "txt")
+        with open(newFilename, 'w', encoding='utf8') as unitcardfile:
+            lines = []
             for row in csvreader:
                 print(row)
-                # line = "{{UnitcardV2|"+
-                        # +"}}"
+                line = ("{{UnitcardV2"+
+                        (("|name=" + row[0]) if row[0] else '') +
+                        (("|cc=" + row[1]) if row[1] else '') +
+                        (("|level=" + row[2]) if row[2] else '') +
+                        (("|Skill=" + row[3]) if row[3] else '') +
+                        (("|aff=" + row[4]) if row[4] else '') +
+                        (("|CR=" + row[5]) if row[5] else '') +
+                        (("|note=" + row[6]) if row[6] else '') +
+                        (("|nickname=" + row[7]) if row[7] else '') +
+                        "}}\n")
+                lines.append(line)
+            lines.append("{{clr}}")
+            unitcardfile.writelines(lines)
     return
 
 def unitcardToCsv(filename):
